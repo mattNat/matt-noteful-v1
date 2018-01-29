d41a5bd712f443378c9d488d8ec96ae1ec668235
mattNat <mattsanpedro76@gmail.com>
2018-01-29 15:31:03 -0800

    finish part 1

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 'use strict';
 
+const express = require('express');
+
 const data = require('./db/notes');
 
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+const app = express();
+app.use(express.static('public'));
+
+app.get('/v1/notes/:id', (req, res) => {
+  console.log((Number(req.params.id)));
+  const input = Number(req.params.id);
+  const findID = data.find(item => item.id === input);
+  console.log(findID);
+  res.json(data);
+});
+
+app.get('/v1/notes', (req, res) => {
+  // console.log(req.query);
+  const {searchTerm} = req.query;
+  // console.log(searchTerm);
+  // console.log(data[0].title);
+  
+  data.forEach(item => console.log(item.title.length));
+  const result = searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data;
+  
+  console.log(result);
+  
+
+  res.json(result);
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
